99b1d61cc63edbbceca9858a7878a844603f6bb1
GER0004 <egor201295@gmail.com>
2026-03-06 13:34:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ menuToggle.addEventListener("click", () => {
 menu.classList.toggle("active")
 
 })
+document.querySelectorAll(".read-more-btn").forEach(button => {
+
+button.addEventListener("click", () => {
+
+const block = button.closest(".text-block")
+
+block.classList.toggle("open")
+
+if(block.classList.contains("open")){
+button.textContent = "Show Less"
+}else{
+button.textContent = "Read More"
+}
+
+})
+
+})
